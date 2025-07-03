079a2d4e51078b8081dabc0ac790b959d2c3c402
Slimey489 <destinyinvective@gmail.com>
2025-07-03 22:07:05 +1200

    Removed unnecessary imports

--- src/main/java/org/sigmaplex/jms/MapPart.java
+++ src/main/java/org/sigmaplex/jms/MapPart.java
@@ -1,7 +1,5 @@
 package org.sigmaplex.jms;
 
-import info.journeymap.shaded.org.eclipse.jetty.util.security.Credential;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
